e66cada4d62b0279afec45e17f1a6e01778c0ff1
XencoLabs Admin <xen@xencolabs.com>
2026-09-22 17:42:50 +0000

    Register decorator using its autoloaded service identity

--- ops/mautic/ImageCrafterGuardBundle/ImageCrafterGuardBundle.php
+++ ops/mautic/ImageCrafterGuardBundle/ImageCrafterGuardBundle.php
@@ -8,8 +8,8 @@ class ImageCrafterGuardBundle extends PluginBundleBase
     public function build(ContainerBuilder $container): void
     {
         parent::build($container);
-        $container->register('imagecrafter.queued_send_guard', QueuedSendGuard::class)
+        $container->register(QueuedSendGuard::class, QueuedSendGuard::class)
             ->setDecoratedService('mailer.messenger.message_handler')
-            ->setArguments([new Reference('imagecrafter.queued_send_guard.inner'), new Reference('imagecrafter.send_guard')]);
+            ->setArguments([new Reference(QueuedSendGuard::class.'.inner'), new Reference('imagecrafter.send_guard')]);
     }
 }
